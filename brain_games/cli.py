88c8084136ab1be26a8b2e252c67f97ed7a133ff
Sergey Golodyaev <gabadysergei@yandex.ru>
2020-12-04 13:01:11 +0300

    linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,10 @@
+
+
+"""welcome user."""
+
 import prompt
 
 
 def welcome_user():
+    """Welcome user function."""
     prompt.string('May I have your name? ')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+"""welcome user."""
+
 from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    """Welcome user function."""
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
