f3abcae7de452fe800a8f20c5cd5875857cf4a54
Sergey Golodyaev <gabadysergei@yandex.ru>
2020-12-09 10:21:19 +0300

    lint

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -27,14 +27,17 @@ def is_current_answer(answer, question):
     return answer_good_yes or answer_good_no
 
 
-def ask_question(round_of_game, count_of_round):
+def ask_question(count_of_round):
     """Welcome user question.
 
     Parameters:
-        round_of_game: round game
-        count_of_round: count.
-    """
+        count_of_round: count
 
+    Returns:
+        round_of_game: round game
+        answer: yes or no
+    """
+    round_of_game = 0
     while round_of_game < count_of_round:
         question = random.randint(1, 1000)
         print('Question: {0}'.format(question))
@@ -45,6 +48,8 @@ def ask_question(round_of_game, count_of_round):
         else:
             break
 
+    return {'answer': answer, 'round_of_game': round_of_game}
+
 
 def start_game(name_user):
     """Welcome user question.
@@ -54,10 +59,16 @@ def start_game(name_user):
     """
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    round_of_game = 1
     count_of_round = 3
-    ask_question(round_of_game, count_of_round)
-    if round_of_game < count_of_round:
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {0}!".format(name_user))
+    answer_question = ask_question(count_of_round)
+    if answer_question['round_of_game'] < count_of_round:
+        if answer_question[0] == 'yes':
+            print("""'{0}' is wrong answer ;(. Correct answer was \
+                  'no'.\n Let's try again, \
+                  {1}!""".format(answer_question['answer'], name_user))
+        else:
+            print("""'{0}' is wrong answer ;(. Correct answer was \
+                  'yes'.\n Let's try again, \
+                  {1}!""".format(answer_question['answer'], name_user))
     else:
         print('Congratulations, {0}!'.format(name_user))
